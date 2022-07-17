585382128ec9c84d7d93ef74955d110b32445d27
18sancheet <sancheetwalekar@gmail.com>
2022-07-17 21:04:37 +0530

    first commit

---
+++ (11) MasterPagesDemo/MasterPagesDemo/Profile.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Profile.aspx.cs" Inherits="MasterPagesDemo.Profile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>This is Profile Page</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Font-Size="X-Large" Text="Upload Profile Picture"></asp:Label>
+&nbsp;&nbsp;&nbsp;<br />
+    <br />
+&nbsp;<asp:FileUpload ID="picupload" runat="server" Font-Size="X-Large" />
+    &nbsp;
+    <br />
+    <br />
+    <asp:Button ID="btnupload" runat="server" Font-Size="X-Large" Height="64px" style="margin-left: 204px" Text="Upload" Width="211px" OnClick="btnupload_Click" />
+    <br />
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Image ID="imgprofile" runat="server" Height="200px" Width="300px" BorderStyle="Ridge" />
+&nbsp;&nbsp;
+<br />
+<asp:Label ID="Label2" runat="server" Font-Size="Large" ForeColor="Red"></asp:Label>
+</asp:Content>
---
+++ (11) MasterPagesDemo/MasterPagesDemo/Profile.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasterPagesDemo
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnupload_Click(object sender, EventArgs e)
+        {
+            //check user has selected the file or not
+            if(picupload.HasFile) //returns true if user has selected the file
+            {
+                if (picupload.PostedFile.ContentType.Equals(@"image/jpeg"))
+                {
+                    try
+                    {
+                        picupload.SaveAs(@"C:\Users\mcamock\source\repos\MasterPagesDemo\MasterPagesDemo\Profilepics\" + picupload.FileName);
+                        Label2.Text = "File uploaded successfully<br>" + picupload.PostedFile.ContentType;
+                        imgprofile.ImageUrl = @"~\Profilepics\" + picupload.FileName;
+                    }
+                    catch (Exception ex)
+                    {
+                        Label2.Text = ex.Message;
+                    }
+                }
+                else
+                {
+                    Label2.Text = "File must be jpeg";
+                }
+            }
+            
+            else
+            {
+                Label2.Text = "You have not selected any file";
+            }
+        }
+    }
+}
